8007e0a228979e5b94d2fc587fbd652b5cf1bed0
Jose Ruas <joseluis.ruas@gmail.com>
2019-11-19 10:37:31 -0300

    create simple front components

--- f-books-library/src/App.js
+++ f-books-library/src/App.js
@@ -1,42 +1,27 @@
 // /client/App.js
 import React, { Component } from 'react';
-import axios from 'axios';
+import { Grid } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Search from '@material-ui/icons/Search';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 class App extends Component {
-  // initialize our state
   state = {
     data: [],
-    id: 0,
-    message: null,
-    intervalIsSet: false,
-    idToDelete: null,
-    idToUpdate: null,
-    objectToUpdate: null,
   };
 
-  // when component mounts, first thing it does is fetch all existing data in our db
-  // then we incorporate a polling logic so that we can easily see if our db has
-  // changed and implement those changes into our UI
   componentDidMount() {
     console.log('here');
     this.getDataFromDb();
-    if (!this.state.intervalIsSet) {
-      let interval = setInterval(this.getDataFromDb, 1000);
-      this.setState({ intervalIsSet: interval });
-    }
   }
 
-  // never let a process live forever
-  // always kill a process everytime we are done using it
-  componentWillUnmount() {
-    if (this.state.intervalIsSet) {
-      clearInterval(this.state.intervalIsSet);
-      this.setState({ intervalIsSet: null });
-    }
-  }
-
-  // our first get method that uses our backend api to
-  // fetch data from our data base
   getDataFromDb = () => {
     fetch('http://localhost:5000/books/')
       .then((data) => data.json())
@@ -45,28 +30,70 @@ class App extends Component {
         this.setState({ data: res.response })});
   };
 
-
-  // here is our UI
-  // it is easy to understand their functions when you
-  // see them render into our screen
   render() {
     const { data } = this.state;
     console.log(data);
     return (
-      <div>
-        <ul>
-          {data.length <= 0
-            ? 'NO DB ENTRIES YET'
-            : data.map((dat) => (
-                <li style={{ padding: '10px' }} key={data.message}>
-                  <span style={{ color: 'gray' }}> id: </span> {dat.id} <br />
-                  <span style={{ color: 'gray' }}> data: </span>
-                  {dat.title}
-                </li>
-              ))}
-        </ul>
-      </div>
+        <div>
+          <Grid container direction="column" spacing={1}>
+            <Grid item container justify="center" alignItems="center" alignContent="center" direction="row" xs={12}>
+              <Grid item container justify="center" sm={2}>
+                <div>SUPERO</div>
+              </Grid>
+              <Grid item sm={8}>
+                  <TextField fullWidth
+                    id="input-with-icon-textfield"
+                    label="Título, autor ou ISBN"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <Search />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+              </Grid>
+              <Grid item container justify="center" sm={2}>
+                  <Button variant="outlined">Buscar</Button>
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <div>YEAR FILTER</div>
+            </Grid>
+            <Grid item xs={12}>
+              {this.simpleTable(data)}
+            </Grid>
+          </Grid>
+        </div>
+        
     );
+  }
+
+  simpleTable(data) {
+    return <Paper width='100%' overflowX='auto'>
+      <Table minWidth='650' aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Título</TableCell>
+            <TableCell align="right">Autor</TableCell>
+            <TableCell align="right">Editora</TableCell>
+            <TableCell align="right">Ano</TableCell>
+            <TableCell align="right">Ações</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(row => (<TableRow key={row.name}>
+            <TableCell component="th" scope="row">
+              {row.title}
+            </TableCell>
+            <TableCell align="right">{row.author_name}</TableCell>
+            <TableCell align="right">{row.publisher_name}</TableCell>
+            <TableCell align="right">{row.year}</TableCell>
+            <TableCell align="right">AAA</TableCell>
+          </TableRow>))}
+        </TableBody>
+      </Table>
+    </Paper>;
   }
 }
 
